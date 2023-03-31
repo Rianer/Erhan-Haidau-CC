8d5bdbb4453cd21796fa2634b4b705a9e3f6e998
Rianer <aerhan2001@gmail.com>
2023-03-31 13:54:36 +0300

    Folders

--- project/app/server.js
+++ project/app/server.js
@@ -1,4 +1,3 @@
-// [START gae_flex_quickstart]
 const express = require("express");
 
 const app = express();
@@ -9,12 +8,10 @@ app.get("/", (req, res) => {
   res.status(200).send("Test!").end();
 });
 
-// Start the server
 const PORT = parseInt(process.env.PORT) || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log("Press Ctrl+C to quit.");
 });
-// [END gae_flex_quickstart]
 
 module.exports = app;
